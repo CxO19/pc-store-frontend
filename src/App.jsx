7e359f727fbab3af07eddb087740736aa45eafeb
CxO19 <mateoortegasaca@gmail.com>
2026-07-20 14:31:16 -0500

    feat: sidebar y layout privado

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import ProductsPage from './pages/ProductsPage'
+import PrivateLayout from './layouts/PrivateLayout'
+import DashboardPage from './pages/private/DashboardPage'
 
 function PrivateRoute({ children }) {
   const token = localStorage.getItem('token')
@@ -18,11 +20,33 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/products" element={
-          <PrivateRoute>
-            <ProductsPage />
-          </PrivateRoute>
-        } />
+        <Route path="/productos" element={<ProductsPage />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <PrivateLayout />
+            </PrivateRoute>
+          }
+        >
+          <Route index element={<DashboardPage />} />
+        </Route>
+
+        <Route
+          path="/admin/*"
+          element={
+            <PrivateRoute>
+              <PrivateLayout />
+            </PrivateRoute>
+          }
+        >
+          <Route path="usuarios" element={<DashboardPage />} />
+          <Route path="productos" element={<DashboardPage />} />
+          <Route path="categorias" element={<DashboardPage />} />
+          <Route path="marcas" element={<DashboardPage />} />
+          <Route path="ordenes" element={<DashboardPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
